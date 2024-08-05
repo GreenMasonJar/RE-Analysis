ee23b6f48cae80c6954978ad6d14745065c41fec
Mason <109998705+GreenMasonJar@users.noreply.github.com>
2024-08-04 21:12:17 -0600

    Server calculations create an object. Object is sent to app.js and displayed after submitting.

--- App/src/App.js
+++ App/src/App.js
@@ -6,10 +6,12 @@ import SubmitButton from './Components/submitButton';
 
 function App() {
   const [test, setTest] = React.useState("");
+  const [results, setResults] = React.useState("Results will shows here");
   const[formData, setFormData] = React.useState({
     homePrice: "",
     downPayment: "",
-    interest: "",
+    interestRate: "",
+    mortgageTerm: "",
     rent: "",
     taxes: "",
     insurance: "",
@@ -46,6 +48,7 @@ function App() {
     const response = await fetch(url, requestOptions)
     const data = await response.json()
     setTest(data.message);
+    setResults(data.calculations);
     console.log(data.message); 
   };
   
@@ -55,7 +58,8 @@ function App() {
       {/* <InputBox value={homePrice} label="House Price" onChange={handleTextChange}/> */}
       <InputBox label = "Home Price: " name="homePrice" value={formData.homePrice} onChange={handleInputChange}/> <br/>
       <InputBox label = "Down Payment (%): " name="downPayment" value={formData.downPayment} onChange={handleInputChange}/><br/>
-      <InputBox label = "Interest Rate: " name="interest" value={formData.interest} onChange={handleInputChange}/> <br/>
+      <InputBox label = "Interest Rate: " name="interestRate" value={formData.interestRate} onChange={handleInputChange}/> <br/>
+      <InputBox label = "Term of Mortgage: " name="mortgageTerm" value={formData.mortgageTerm} onChange={handleInputChange}/> <br/>
       <InputBox label = "Total Rental Income: " name="rent" value={formData.rent} onChange={handleInputChange}/> <br/>
       <InputBox label = "Property Tax: " name="taxes" value={formData.taxes} onChange={handleInputChange}/> <br/>
       <InputBox label = "Property Insurance: " name="insurance" value={formData.insurance} onChange={handleInputChange}/> <br/>
@@ -63,6 +67,9 @@ function App() {
       <InputBox label = "HOA Fees: " name="hoa" value={formData.hoa} onChange={handleInputChange}/> <br/>
       <InputBox label = "Property Management: " name="management" value={formData.management} onChange={handleInputChange}/> <br/>
       <SubmitButton value="submit" label="Submit or something" onClick={handlesSubmit}/>
+      Total Price of Home: ${results.homePrice} <br/>
+      Down Payment: ${results.downPayment} <br/>
+      Monthly Mortgage payment: ${results.mortgagePayment}
     </div>
   );
 }
--- servers/server.js
+++ servers/server.js
@@ -5,13 +5,27 @@ app.use(express.json());
 
 app.post('/rental', (req, res) => {
     const propertyData = req.body;
-    console.log(req.body);
-  
-    // Save the data of user that was sent by the client
-  
-    // Send a response to client that will show that the request was successfull.
+    
+    //DownPayment
+    var dp = propertyData.homePrice*(propertyData.downPayment/100)
+    console.log("Downpayment: ", dp);
+
+    //loan amount after down payment
+    var loan = propertyData.homePrice - dp;
+    
+    //Calculate Mortgage Payment using loan amount, interest, and mortgage term
+    var mortgage = (loan*((propertyData.interestRate/100)/12))/(1-(1+((propertyData.interestRate/100)/12))**(-12*propertyData.mortgageTerm))
+    console.log(mortgage)
+    
+    const data = {
+      homePrice: propertyData.homePrice, 
+      downPayment: dp, 
+      mortgagePayment: mortgage
+    }
+
     res.send({
-      message: "Data received"
+      message: "Data received",
+      calculations: data
     });
   });
 
